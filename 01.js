09aa5e7683812c7d25cd52b7c54049defb12a920
nataliko7 <65019286+nataliko7@users.noreply.github.com>
2020-05-14 10:01:59 +0400

    homework

---
+++ 01.js
@@ -0,0 +1,19 @@
+//lesson 01
+/*
+01.js
+ Autor: Natalia
+Date: 14.05
+*/
+
+let i = 1;
+let sum = 0;
+while(i<10) { 
+sum = sum + i;
+console.log(sum);
+}
+
+let i = 3;
+let sum = 0;
+for ( i = 3 ; i < 50 ; i +=3; ) {
+    console.log(i)
+}
